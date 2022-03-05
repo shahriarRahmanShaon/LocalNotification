1ffed768b152811b03ba65512e3febab528a47e9
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-06 00:30:38 +0600

    unNotificationCenter setup

--- LocalNotification/ContentView.swift
+++ LocalNotification/ContentView.swift
@@ -9,8 +9,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        VStack{
+            Button{
+                UNUserNotificationCenter.current().requestAuthorization(options: [.sound, .badge,.alert]) { success, error in
+                    if success {
+                        print("Notification Successful")
+                    }
+                    if let error = error {
+                        print(error.localizedDescription)
+                    }
+                }
+            } label: {
+                Text("Request Permission")
+                    .padding()
+                    .background(.cyan)
+                    .foregroundColor(.white)
+                    .cornerRadius(50)
+            }
+            
+            Button{
+                let content = UNMutableNotificationContent()
+                content.title = "Bring me food"
+                content.subtitle = "Hourly reminder to eat something"
+                content.sound = UNNotificationSound.default
+                
+                let trigger = UNTimeIntervalNotificationTrigger(timeInterval: 5, repeats: false)
+                
+                let request = UNNotificationRequest(identifier: UUID().uuidString, content: content, trigger: trigger)
+                
+                UNUserNotificationCenter.current().add(request)
+            } label:{
+                Text("Schedule Notification")
+                    .padding()
+                    .background(.orange)
+                    .foregroundColor(.white)
+                    .cornerRadius(50)
+            }
+
+        }
     }
 }
 
