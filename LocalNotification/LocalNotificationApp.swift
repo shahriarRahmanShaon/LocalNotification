927f977ba03adf2ede93e4db3241d3e6d4a19bac
shahriarRahmanShaon <shaon463@gmail.com>
2022-03-06 00:08:07 +0600

    Initial Commit

---
+++ LocalNotification/LocalNotificationApp.swift
@@ -0,0 +1,17 @@
+//
+//  LocalNotificationApp.swift
+//  LocalNotification
+//
+//  Created by sergio shaon on 6/3/22.
+//
+
+import SwiftUI
+
+@main
+struct LocalNotificationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
